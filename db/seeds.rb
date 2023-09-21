682bd049fccbd0fc530a7ced3df58e54d46e74fa
Pratik Manoj Desai <pratikdesai@Pratiks-MacBook-Pro.local>
2023-09-21 00:19:11 -0500

    movies added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '16-Jul-2010'},
+  {:title => 'Joker', :rating => 'PG-13',
+    :release_date => '04-Oct-2019'}
 ]
 
 more_movies.each do |movie|
